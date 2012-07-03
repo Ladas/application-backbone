1a86b187cfac3ce5f571530099eea1cb10fcdba2
Ladas <ladislav.smola@it-logica.cz>
2012-07-03 19:18:04 +0200

    non ajax urls for links in the table

--- app/assets/javascripts/backbone_js/ladas_table_for.js
+++ app/assets/javascripts/backbone_js/ladas_table_for.js
@@ -1,13 +1,16 @@
-function apply_modifiers_of_the_table(area)
-{
+function apply_modifiers_of_the_table(area) {
 //    console.log("ua")
-    area.find("a").each( function (){
-        var a_obj =  $(this);
-        if (a_obj.data("tr_class"))
-        {
+    area.find("a").each(function () {
+        var a_obj = $(this);
+        if (a_obj.data("tr_class")) {
 //            console.log("jooo")
 //            console.log(a_obj.data("tr_class"))
             a_obj.parents("tr").addClass(a_obj.data("tr_class"));
+        }
+        if (a_obj.data("td_class")) {
+            //            console.log("jooo")
+            //            console.log(a_obj.data("tr_class"))
+            a_obj.parents("td").addClass(a_obj.data("td_class"));
         }
     })
 }

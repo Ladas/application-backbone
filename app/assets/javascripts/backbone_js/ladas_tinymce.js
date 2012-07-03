b6634e4507673d01b1230d8f0bf845d8d12d5b8c
Ladas <ladislav.smola@it-logica.cz>
2012-07-03 18:24:50 +0200

    spellchecker utf-8 error, spellcchecker deleted

--- app/assets/javascripts/backbone_js/ladas_tinymce.js
+++ app/assets/javascripts/backbone_js/ladas_tinymce.js
@@ -26,13 +26,13 @@
              theme_advanced_resizing:true,
              */
             theme:"advanced",
-            plugins:"pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+            plugins:"pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
 
             // Theme options
             //theme_advanced_buttons1:"save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
             theme_advanced_buttons1:"newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect,fontselect,fontsizeselect",
             theme_advanced_buttons2:"cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,code",
-            theme_advanced_buttons3:"tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media",
+            theme_advanced_buttons3:"tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,media",
             theme_advanced_buttons4:"cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,|,insertdate,inserttime,preview,|,forecolor,backcolor,|,advhr,|,print,|,fullscreen",
             theme_advanced_toolbar_location:"top",
             theme_advanced_toolbar_align:"left",

3b9c6974b24f8f9057ce9df3b7d7639813d38e90
Šlemr Martin <martin.slemr@it-logica.cz>
2012-08-22 10:34:43 +0200

    rozsireni datafiles o sub_type

--- lib/view_mixins/datafiles_for.rb
+++ lib/view_mixins/datafiles_for.rb
@@ -1,7 +1,7 @@
 module ViewMixins
   module DatafilesFor
-    def datafiles_for(object, tiny_mce_selector = ".datafile_tinymce", can_upload = true)
-      render :partial => '/helpers/build_datafiles', :layout => false, :locals => {:object => object, :tiny_mce_selector => tiny_mce_selector, :can_upload => can_upload}
+    def datafiles_for(object, tiny_mce_selector = ".datafile_tinymce", can_upload = true, sub_type = nil)
+      render :partial => '/helpers/build_datafiles', :layout => false, :locals => {:object => object, :tiny_mce_selector => tiny_mce_selector, :can_upload => can_upload, :sub_type => sub_type}
     end
   end
 end

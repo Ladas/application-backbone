f95ec63c5677d86cb3a139c8b31a7566ced988b5
Ladas <ladislav.smola@it-logica.cz>
2012-09-12 19:59:58 +0200

    helper for selected vaulues from custom filter

--- lib/controller_mixins/table_settings_interface.rb
+++ lib/controller_mixins/table_settings_interface.rb
@@ -4,5 +4,30 @@ module ControllerMixins
 
     require "table_settings.rb"
 
+
+    ##
+    # Selected values from custom filter, it checks params and session
+    ##
+    def selected_values(form_id, path, default = [])
+      par = params
+      path.each do |p|
+        break if par.blank?
+        par = par[p.to_s]
+      end
+      selected = par unless par.blank?
+
+      if selected.blank?
+        par = session[form_id+"_params"]
+
+        path.each do |p|
+          break if par.blank?
+          par = par[p.to_s] unless par[p.to_s].blank?
+        end
+        selected = par
+      end
+      selected = selected.blank? ? default : selected
+
+      selected
+    end
   end
 end
